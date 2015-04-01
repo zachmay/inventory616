abf4df7fee04da48267a2d1f235da5edcf36aa2a
liudw9094 <luxastom@gmail.com>
2015-04-01 14:41:09 -0700

    new file:   tests/ApiInventory_ManageTest.php 	deleted:    tests/ApiInventoryTest.php

---
+++ tests/ApiInventory_ManageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class ApiInventoryManageTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function testGet()
+	{
+		$response = $this->call('GET', 'api/inventory');
+		$this->assertResponseStatus(200, $response->getStatusCode());
+        	$this->assertEquals('text/html', $response->headers->get('content-type'));
+	}
+	public function testPost()
+	{
+		$response = $this->call('POST', 'api/inventory');
+		$this->assertResponseStatus(201, $response->getStatusCode());
+        	$this->assertEquals('application/json', $response->headers->get('content-type'));
+	}
+	public function test_typesGet()
+	{
+		$response = $this->call('GET', 'api/inventory/types');
+		$this->assertResponseStatus(200, $response->getStatusCode());
+        	$this->assertEquals('text/html', $response->headers->get('content-type'));
+	}
+}
