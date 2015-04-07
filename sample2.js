bfb33b3c0d736c54ef571776847b4aa1c115c7e9
harika05 <alpharika@gmail.com>
2015-04-07 19:18:53 -0400

    UI for facilities

---
+++ sample2.js
@@ -0,0 +1,77 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*jslint undef: true, white: true, browser: true */
+/*global Backbone */
+/*jslint nomen: true */
+/*global _ */
+/*global $ */
+/*global define */
+/* jslint unused: false */
+var Create = Backbone.Model.extend( {
+	defaults: {
+	building : '',
+	roomno  : ''
+},
+	urlRoot: 'buildings/'
+} );
+var create = new Create();
+var Creates = Backbone.Collection.extend({});
+var create1 = new Create( {
+	building: 'Powell1 ',
+	roomno: '102'
+});
+var create2 = new Create( {
+	building: ' Powell2',
+	roomno: '103'
+});
+var creates = new Creates([create1, create2]);
+
+var CreateView = Backbone.View.extend( {
+	model: create,
+	tagname: 'td',
+	initialize: function() {
+		'use strict';
+		console.log('there');
+		
+		this.template = _.template($('#facilities-template').html());
+		
+		$(this.el).html(this.template(this.model.toJSON()));
+	}
+});
+
+var CreatesView = Backbone.View.extend( {
+	model: creates,
+	el: $('#facilities'),
+	initialize: function() {
+		'use strict';
+		this.model.on('add', this.render, this);
+	},
+	render: function() {
+		'use strict';
+		 var self;
+		 self = this;
+		$(this.el).html('');
+		console.log('this');
+		_.each(this.model.toArray(), function(create) {
+			alert(create.get('building') + ' ' + create.get('roomno'));
+			self.$el.append((new CreateView({model: create})).render.$el);
+		});
+		return this;
+	}
+});
+
+var createsView = new CreatesView();
+
+$(document).ready(function() {
+	'use strict';
+	$('.add-create').on('click', function() {
+		var create = new Create( {
+			building: $('.building-input').val(),
+			roomno: $('.roomno-input').val()
+		});
+		$('.building-input').val('');
+		$('.roomno-input').val('');
+		creates.add(create);
+		console.log(create.toJSON());
+		
+	});
+});
