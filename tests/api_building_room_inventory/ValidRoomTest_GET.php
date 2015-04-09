47ae08f2e864ae165dfbca7d1e68348b9a87596c
liudw9094 <luxastom@gmail.com>
2015-04-08 18:23:41 -0700

    modified:   tests/api_building_room_inventory/ValidRoomTest_GET.php

--- tests/api_building_room_inventory/ValidRoomTest_GET.php
+++ tests/api_building_room_inventory/ValidRoomTest_GET.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+
+use App\Building;
+use App\Item;
+use App\ItemType;
+use App\Room;
+use App\User;
+
+
 class ValidRoomTest_GET extends TestCase {
 
 	/**
@@ -7,14 +16,70 @@ class ValidRoomTest_GET extends TestCase {
 	 *
 	 * @return void
 	 */
+	private $room;
+	private $building;
+
+	public function setUp()
+	{
+		parent::SetUp();
+		// Setup assumptions.
+		Model::unguard();
+		$building = Building::where('id', '=', 101)->first();
+		if(is_null($building))
+		{
+			$building = Building::create(['id' => '101', 'name' => 'test_bld', 'description' => 'test_bld']);
+		}
+		//echo($building->toJson());
+
+		$room = Room::where('id', '=', 3320,
+						'and', 'building_id','=', $building->id)->first();
+		if(is_null($room))
+		{
+			$room = Room::create(['id' => '3320', 'name' => 'test_room', 'description' => 'test_room', 'building_id' => $building->id]);
+		}
+		//echo($room->toJson());
+		$this->room = $room;
+		$this->building = $building;
+		//fwrite(STDOUT, __METHOD__ . "\n");
+	}
+
+	public function tearDown()
+	{
+		// Cleanup assumptions.
+		try
+		{
+			if(isset($this->room))
+			{
+				$this->room->Delete();
+			}
+			if(isset($this->building))
+			{
+				$this->building->Delete();
+			}
+		}
+		catch(Exception $e)
+		{
+			echo($e);
+			echo("\n");
+		}
+		finally
+		{
+			parent::tearDown();
+			//fwrite(STDOUT, __METHOD__ . "\n");
+		}
+	}
+
 	public function testGet()
 	{
 		$response = $this->call('GET', 'http://192.168.33.99/api/buildings/101/rooms/3320/inventory');
-
-        // Let's confirm some of the response headers.
-        //
+		// confirm some of the response headers.
 		$this->assertResponseStatus(200, $response->getStatusCode());
 		$this->assertEquals('application/json', $response->headers->get('content-type'));
-		$body = json_decode($response->getContent());
+		// convert json to an array rather than an object.
+		$body = json_decode($response->getContent(), true);
+		// confirm the content is a correct answer.
+		$this->assertEquals($body, $this->room->toArray());
 	}
+
+
 }
